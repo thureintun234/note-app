6ad44617eaf8949f3ca5a50ee2e44979047cb91f
thureintun234 <thurein12456@gmail.com>
2021-09-14 09:50:04 +0630

    database config

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Note = require('./models/note')
 
-app.use(express.static('build'))
-app.use(express.json())
-app.use(cors())
-
-
 // morgon token customize
 morgan.token('data', (req, res) => {
   return JSON.stringify(req.body)
@@ -17,7 +12,12 @@ morgan.token('data', (req, res) => {
 
 // morgon logger
 const logger = morgan(':method :url :status :res[content-length] - :response-time ms :data')
+
+app.use(express.static('build'))
+app.use(express.json())
+app.use(cors())
 app.use(logger)
+
 
 app.get('/api/notes', (req, res) => {
   Note.find({}).then(notes => {
@@ -25,20 +25,27 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-  Note.findById(req.params.id).then(note => {
-    res.json(note)
-  })
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then(note => {
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(err => next(err))
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter(n => n.id !== id)
-
-  res.status(204).end()
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(err => next(err))
 })
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
   const body = req.body
 
   if (!body.content) {
@@ -51,10 +58,46 @@ app.post('/api/notes', (req, res) => {
     date: new Date()
   })
 
-  note.save().then(savedNote => {
-    res.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormatedNote => res.json(savedAndFormatedNote))
+    .catch(err => next(err))
 })
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then(updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(err => next(err))
+})
+
+// unknown enpoint middleware
+const unknownEndPoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndPoint)
+
+
+// malformateed id middleware
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).send({ error: error.message })
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 const port = process.env.PORT || 3001
 app.listen(port, () => {
